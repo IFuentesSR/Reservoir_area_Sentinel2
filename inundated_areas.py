79162cf4730d7c93f60e8b344133955a3beef36d
root <ignacio.fuentes.sanroman@gmail.com>
2022-01-24 19:04:20 -0300

    created croping display function and intersect condition

--- inundated_areas.py
+++ inundated_areas.py
@@ -7,13 +7,36 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.ndimage.interpolation import shift
 import shapefile
-from shapely.geometry import Polygon, MultiPolygon
+from shapely.geometry import Polygon, MultiPolygon, box
 
 
 def get_feature(shape_path, dam):
     shape = shapefile.Reader(shape_path)
     feature = [n for n in shape.shapeRecords() if n.record.DAM_LOTPLA == dam][0]
     return feature
+
+
+def single_band(path):
+    inner_path = os.path.join(path,
+                              'GRANULE',
+                              os.listdir(os.path.join(path, 'GRANULE'))[0])
+    band_paths = [os.path.join(os.path.join(inner_path, 'IMG_DATA'), n)
+                  for n in os.listdir(os.path.join(inner_path, 'IMG_DATA'))]
+    bands = ['B01', 'B02', 'B04', 'B05', 'B08', 'B8A', 'B09', 'B10', 'B11', 'B12']
+    resampled_bands = [1, 2, 3, 4, 5, 8, 9]
+    sorted_bands = [[m for m in band_paths if n in m] for n in bands]
+    sorted_bands = [n for l in sorted_bands for n in l]
+    single_band_path = sorted_bands[1]
+    return single_band_path
+
+
+def raster_intersects(raster_path, shape_path, dam):
+    single_band_path = single_band(raster_path)
+    fil = rasterio.open(single_band_path)
+    box_raster = box(*fil.bounds)
+    fea = get_feature(shape_path, dam)
+    poly = Polygon(fea.shape.__geo_interface__['coordinates'][0])
+    return box_raster.intersects(poly)
 
 
 def fea_area(feature):
@@ -44,10 +67,6 @@ def croping(img_band, feature):
                          'height':out_image.shape[1]})
         src.close()
     return out_image, out_meta
-
-
-def croping_display():
-    ToDo
 
 
 def get_windows(shape, reservoir, img):
@@ -121,13 +140,13 @@ def get_band_paths(outer_path):
 
 
 def water_index(array):
-    array = array / 10000
-    blue = array[0,:,:,0]
-    green = array[0,:,:,1]
-    red = array[0,:,:,2]
-    nir = array[0,:,:,3]
-    swir1 = array[0,:,:,4]
-    swir2 = array[0,:,:,5]
+    agg_array = array / 10000
+    blue = agg_array[0,:,:,0]
+    green = agg_array[0,:,:,1]
+    red = agg_array[0,:,:,2]
+    nir = agg_array[0,:,:,3]
+    swir1 = agg_array[0,:,:,4]
+    swir2 = agg_array[0,:,:,5]
     water_ix = 1.7204 + 171 * green + 3 * red - 70 * nir - 45 * swir1 - 71 * swir2
     return water_ix
 
@@ -149,3 +168,25 @@ def inundated_area_calculation(outer_path, shapefile, dam):
     water_arr = np.where(masked_water > water_thresh, 1, 0)
     inundated_area = np.sum(water_arr) * pixel_area
     return dam, pd.to_datetime(outer_path[11:19], format='%Y%m%d'), inundated_area, unmasked_ratio
+
+
+def croping_display(outer_path, shape, dam):
+    fea = get_feature(shape, dam)
+    poly = Polygon(fea.shape.__geo_interface__['coordinates'][0])
+    buffer = poly.buffer(300)
+    coords = buffer.bounds
+    coords = [(coords[0], coords[1]), (coords[2], coords[1]),
+              (coords[2], coords[3]), (coords[0], coords[3])]
+    outer_poly = Polygon(coords)
+    paths = get_band_paths(outer_path)
+    date = outer_path[11:19]
+    cloud_mask, mask_meta = croping(paths[-1], outer_poly)
+    band_list = [croping(n, outer_poly) for n in paths[:-1]]
+    agg_array = np.stack([n[0] for n in band_list]).transpose(1, 2, 3, 0)
+    mask = np.where(agg_array[0,:,:,0] == 0, 0, 1)
+    water_ix = water_index(agg_array)
+    masked_water = np.where(mask == 0, np.nan, water_ix)
+    masked_water = np.where(cloud_mask[0, :, :] > 0, np.nan, masked_water)
+    mask_meta.update({'dtype':'float32', 'driver':'GTiff', 'count':1})
+    with rasterio.open(os.path.join('tmp', '{}_{}.tif'.format(dam, date)), "w", **mask_meta) as dest:
+        dest.write(masked_water, indexes=1)
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from masking_cloud_shadows import save_mask
-from inundated_areas import inundated_area_calculation, croping_display
+from inundated_areas import inundated_area_calculation, croping_display, raster_intersects
 
 
 outer_path = 'S2A_MSIL1C_20220102T001111_N0301_R073_T55JGG_20220102T013405.SAFE'
@@ -8,4 +8,12 @@ dam = 's242-1'
 
 
 save_mask(outer_path)
-dam, date, inundated_area, unmasked_ratio = inundated_area_calculation(outer_path, shapefile_path, dam)
+if raster_intersects(outer_path, shapefile_path, dam):
+    dam, date, inundated_area, unmasked_ratio = inundated_area_calculation(outer_path, shapefile_path, dam)
+    croping_display(outer_path, shapefile_path, dam)
+else:
+    print("polygon doesn't intercept rater")
+
+
+"""ToDo import sys and pass arguments from terminal to run.py, maybe create
+class for inundated_areas"""
