074689de15a5ff3a801c1a89895c8dbf1e945b28
IFuentesSR <ignacio.fuentes.sanroman@gmail.com>
2022-02-10 19:04:10 -0300

    cleaning  run.py

--- run.py
+++ run.py
@@ -2,8 +2,7 @@ import sys
 
 from masking_cloud_shadows import save_mask
 from inundated_areas import inundated_area_calculation, croping_display, raster_intersects
-# print(sys.version)
-# !pip install pyshp
+
 
 if len(sys.argv) != 4:
     print('run.py requires three arguments:\n\
@@ -15,11 +14,6 @@ else:
     shapefile_path = sys.argv[2]
     dam = sys.argv[3]
 
-    # outer_path = 'S2A_MSIL1C_20220102T001111_N0301_R073_T55JGG_20220102T013405.SAFE'
-    # shapefile_path = "../namoi_dams2.shp"
-    # dam = 's242-1'
-
-
     save_mask(outer_path)
     if raster_intersects(outer_path, shapefile_path, dam):
         dam, date, inundated_area, unmasked_ratio = inundated_area_calculation(outer_path, shapefile_path, dam)
@@ -27,8 +21,11 @@ else:
         with an unmasked ratio of {}'.format(dam, inundated_area, date, unmasked_ratio))
         croping_display(outer_path, shapefile_path, dam)
     else:
-        print("polygon doesn't intercept rater")
+        print("polygon doesn't intercept raster")
+
+    # outer_path = 'S2A_MSIL1C_20220102T001111_N0301_R073_T55JGG_20220102T013405.SAFE'
+    # shapefile_path = "../namoi_dams2.shp"
+    # dam = 's242-1'
 
 
-"""ToDo import sys and pass arguments from terminal to run.py, maybe create
-class for inundated_areas"""
+"""ToDo maybe create class for inundated_areas"""
